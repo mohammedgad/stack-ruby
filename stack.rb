6c7a3bee6527f363e99548cb2015d5d209feb309
Mohammed Gad <mohammedgad7@gmail.com>
2018-06-28 12:04:06 -0700

    add pop func, max array

--- stack.rb
+++ stack.rb
@@ -2,33 +2,43 @@ class Stack
 
   def initialize()
     @arr = Array.new
-    @ind = -1
-    @max = 0
+    @max = Array.new
   end
 
   def push(n)
-    @ind += 1
-    @arr[@ind] = n
-    @max = n unless @max > n
-  end
-
-  def pop
-    @arr.pop
-  end
-
-  def isEmpty
-    if @ind == 0 || @ind == -1
-      return TRUE
-    else
-      return FALSE
+    @arr.push n
+    if @max.length == 0 || @max.last < n
+      @max.push(n)
     end
   end
 
+  def pop
+    if @arr.pop == @max.last
+      @max.pop
+    end
+  end
+
+  def empty?
+    !!(@arr.length == 0)
+  end
+
   def max
-    puts @max
+    puts @max.last
   end
 
   def display
     puts @arr
   end
 end
+
+
+s = Stack.new()
+s.push(7)
+s.push(8)
+s.push(8)
+s.push(9)
+s.push(0)
+s.display
+puts "/n---------------------------------------/n"
+s.max
+puts s.empty?
